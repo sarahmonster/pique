6d7323ac700dbcdef36442dd33418bbd4dcd2f93
sarah <sarah@triggersandsparks.com>
2015-07-25 11:33:59 +0000

    Pique: Set up frontpage template to pull all sub-paages.
    This sets up the initial skeleton for our scrolling single-page, allowing
    users dynamic control over each individual "panel" and their order.

--- template-front.php
+++ template-front.php
@@ -1,14 +1,60 @@
 <?php
 /**
  * Template Name: Front Page
+ * This template drives our scrolling one-page site.
  *
  * @package Pique
  */
-get_header(); ?>
+get_header();
+
+/**
+ *
+ * We start by generating a new WP Query that will hold all of our top-level pages.
+ * All child pages of our current page will be included.
+ *
+ */
+$args = array (
+  'post_type'     => 'page',
+  'post_parent'   => get_the_ID(), // For child pages of homepage
+  //'post_parent' => 0, // For all top-level pages
+  'orderby'       => 'menu_order',
+  'order'         => 'ASC',
+);
+
+$query = new WP_Query( $args );
+?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+      <?php if ( $query->have_posts() ) : ?>
+
+        <?php // Spin up a Loop!
+          $count = 1;
+          while ( $query->have_posts() ) : $query->the_post();
+          $slug = $post->post_name;
+        ?>
+
+          <div id="<?php echo $slug; ?>" class="pique-panel" data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
+
+            <h2><?php the_title(); ?></h2>
+
+            <?php the_content(); ?>
+
+          </div>
+
+        <?php
+          $count++;
+          endwhile;
+        ?>
+
+        <?php wp_reset_postdata(); ?>
+
+      <?php else : ?>
+        <p><?php _e( 'Welcome to Pique! To start setting up your site, add a page as a child of your homepage.', 'pique' ); ?></p>
+      <?php endif; ?>
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 			<?php get_template_part( 'components/content', 'hero' ); ?>
 			<?php get_template_part( 'components/testimonials' ); ?>
 		</main>
