0bf8cf272550ae8ae3fc8ef5f1e47a02f2b37e87
sarah <sarah@triggersandsparks.com>
2015-07-26 23:58:05 +0000

    Remove link from panel title and center-align.

--- components/content-front.php
+++ components/content-front.php
@@ -19,7 +19,7 @@ endif;
 	<?php endif; ?>
 	<div class="pique-panel-content">
 		<header class="entry-header">
-			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 			<?php if ( 'post' == get_post_type() ) : ?>
 			<div class="entry-meta">
