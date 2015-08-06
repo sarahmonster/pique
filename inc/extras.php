afd8be00aef681e758a2536ca9e2172568d10269
sarah <sarah@triggersandsparks.com>
2015-08-06 13:57:23 +0100

    Modify navigation walker to use hash links for sub-pages of the front page.

--- inc/extras.php
+++ inc/extras.php
@@ -36,75 +36,89 @@ class Pique_Menu extends Walker_Nav_Menu {
 
     function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 )  {
 
-        global $wp_query;
+      global $wp_query;
+      global $post;
 
-            // Get the locations of nav menus
-            $theme_locations = get_nav_menu_locations();
+      // Get the locations of nav menus
+      $theme_locations = get_nav_menu_locations();
 
-            // Get the menu object of the current nav menu based on the returned theme location
-            $menu_obj = get_term( $theme_locations[$args->theme_location], 'nav_menu' );
+      // Get the menu object of the current nav menu based on the returned theme location
+      $menu_obj = get_term( $theme_locations[$args->theme_location], 'nav_menu' );
 
-            if( !isset( $this->current_menu ) ) {
-                $this->current_menu = wp_get_nav_menu_object( $menu_obj->term_id );
-            }
+      if( !isset( $this->current_menu ) ) {
+          $this->current_menu = wp_get_nav_menu_object( $menu_obj->term_id );
+      }
 
-            // Determine a break point for our menu
-            $menu_items = wp_get_nav_menu_items( $menu_obj->term_id );
-            if ( !isset ( $this->top_level_count ) ) {
-            	$this->top_level_count = 0;
-					    foreach ( $menu_items as $menu_item ) {
-					    		if ( 0 == $menu_item->menu_item_parent ) {
-					    				$this->top_level_count++;
-					    		}
-					    }
+      // Determine a break point for our menu
+      $menu_items = wp_get_nav_menu_items( $menu_obj->term_id );
+      if ( !isset ( $this->top_level_count ) ) {
+      	$this->top_level_count = 0;
+		    foreach ( $menu_items as $menu_item ) {
+		    		if ( 0 == $menu_item->menu_item_parent ) {
+		    				$this->top_level_count++;
+		    		}
+		    }
 
-	            if ( !isset( $this->break_point ) ) {
-	                $this->break_point = ceil( $this->top_level_count / 2 );
-	            }
-
-	            $iterator = 0;
-	            foreach ( $menu_items as $menu_item ) {
-				    		if ( 0 == $menu_item->menu_item_parent ) {
-				    				if ( $iterator == $this->break_point ) {
-				    					  $this->id_to_split_on = $menu_item->ID;
-				    				}
-				    				$iterator++;
-				    		}
-					    }
-					   }
-
-
-        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-
-        $class_names = $value = '';
-
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-        $classes[] = 'menu-item-' . $item->ID;
-
-        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-        $class_names = ' class="' . esc_attr( $class_names ) . '"';
-
-        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-        $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-
-        if ( $this->id_to_split_on === $item->ID ) {
-            $output .= $indent . '</li></span><span class="pique-split-nav"><li' . $id . $value . $class_names .'>';
-        } else {
-            $output .= $indent . '<li' . $id . $value . $class_names . '>';
+        if ( !isset( $this->break_point ) ) {
+            $this->break_point = ceil( $this->top_level_count / 2 );
         }
 
-        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $iterator = 0;
+        foreach ( $menu_items as $menu_item ) {
+	    		if ( 0 == $menu_item->menu_item_parent ) {
+    				if ( $iterator == $this->break_point ) {
+    					$this->id_to_split_on = $menu_item->ID;
+    				}
+    				$iterator++;
+	    		}
+		    }
+		  }
 
-        $item_output = $args->before;
-        $item_output .= '<a'. $attributes .'>';
-        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-        $item_output .= '</a>';
-        $item_output .= $args->after;
+      $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+      $class_names = $value = '';
+
+      $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+      $classes[] = 'menu-item-' . $item->ID;
+
+      $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+      $class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+      $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+      $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+      if ( $this->id_to_split_on === $item->ID ) {
+        $output .= $indent . '</li></span><span class="pique-split-nav"><li' . $id . $value . $class_names .'>';
+      } else {
+        $output .= $indent . '<li' . $id . $value . $class_names . '>';
+      }
+
+      // Get the IDs for our target page (the page we're linking to), the parent of our target, and current page
+      $target_page = $item->object_id;
+      $target_parent = get_post( $target_page )->post_parent;
+      $current_page = $post->ID;
+
+      // Check to see if our target page's parent page is the front page. If so, we'll want to use a hash link.
+      if ( 0 !== $target_parent && 'page-templates/template-front.php' === get_page_template_slug( $target_parent ) ) :
+        if ( $current_page === $target_parent ):
+          $item->url = '#post-' . $target_page;
+        else:
+          $item->url = get_page_link( $target_parent ) . '#post-' . $target_page;
+        endif;
+      endif;
+
+      $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+      $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+      $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+      $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+      $item_output = $args->before;
+      $item_output .= '<a'. $attributes .'>';
+      $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+      $item_output .= '</a>';
+      $item_output .= $args->after;
+
+      $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
 }
 
