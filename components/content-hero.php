82dfc2a98c7df71c934c5be5e34195601b2c12e6
sarah <sarah@triggersandsparks.com>
2015-08-12 23:24:01 +0100

    Position navbar at bottom of screen, logo/site title in center of hero panel.
    Spacing will need to be adjusted and tweaked for different screen sizes,
    but right now I'm just trying to get the initial idea up & running.

--- components/content-hero.php
+++ components/content-hero.php
@@ -6,21 +6,12 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'pique-panel pique-hero' ); ?> data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'pique-panel pique-hero' ); ?> data-slide="1" data-stellar-background-ratio="0.5">
   <?php if ( has_post_thumbnail() ) :
     $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'pique-hero' ); ?>
     <div class="pique-panel-background" style="background-image:url(<?php echo esc_url( $thumbnail[0] ); ?>)"></div>
   <?php endif; ?>
   <div class="pique-panel-content">
-    <header class="entry-header">
-      <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-      <?php if ( 'post' == get_post_type() ) : ?>
-      <div class="entry-meta">
-        <?php pique_posted_on(); ?>
-      </div><!-- .entry-meta -->
-      <?php endif; ?>
-    </header><!-- .entry-header -->
 
     <div class="entry-content">
 
