a2d1b1df0437ffac1411b6996dbb3210d0f51431
sarah <sarah@triggersandsparks.com>
2015-09-30 19:57:54 +0100

    Use a wider custom header image.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -25,7 +25,7 @@ function pique_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'pique_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
+		'width'                  => 1140,
 		'height'                 => 250,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'pique_header_style',
