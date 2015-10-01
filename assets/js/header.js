036ec9895e5a4a62068e753482c1e8cc2d181c32
sarah <sarah@triggersandsparks.com>
2015-10-01 14:36:25 +0100

    Arrange menu and logo on mobile screens.

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,9 +5,25 @@
  */
 
 ( function( $ ) {
-  $(window).on("resize", function () {
-    var piqueHeaderHeight = $('#pique-header-image').height();
-    var piqueNavHeight = $('#primary-menu').height();
-    $('#masthead').css('height', piqueHeaderHeight + piqueNavHeight);
-  }).resize();
+
+	// Run every time the window is resized
+	$(window).on("resize", function () {
+
+		// If we're on the homepage, we'll handle things a little differently
+		if ($('body').hasClass('page-template-page-templatestemplate-front-php')) {
+			// For teensy little mobile screens, we want to position the nav before the header elements. Let's do it!
+			var piqueBrandingHeight = $('#masthead').find('.site-branding').height() + 20;
+			var piqueHeroContent = $('#pique-hero').find('.pique-panel-content');
+			$(piqueHeroContent).css('margin-top', piqueBrandingHeight);
+
+		// If we're not on the front page, we'll measure the height of the header
+		// image and the height of our nav to determine our overall header height.
+		} else {
+			var piqueHeaderHeight = $('#pique-header-image').height();
+			var piqueNavHeight = $('#primary-menu').height();
+			$('#masthead').css('height', piqueHeaderHeight + piqueNavHeight);
+		}
+
+	}).resize(); // fire the resize event at load to size & rearrange elements as needed
+
 } )( jQuery );
