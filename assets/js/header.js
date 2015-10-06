5ebee207abaf5babd30acec51623dd0deccf864f
sarah <sarah@triggersandsparks.com>
2015-10-06 13:55:53 +0100

    Generally try to improve look of homepage.

--- assets/js/header.js
+++ assets/js/header.js
@@ -15,7 +15,7 @@
 		// Make sure we're not on the homepage, since that handles stuff differently
 		if (!$('body').hasClass('page-template-page-templatestemplate-front-php')) {
 
-			// Is this a small screen? Okay, we should be using a priority+ navigation approach
+			// Is this a small screen? Okay, we should be making it a little smaller (no need to account for our nav height here)
 			if ( windowWidth < 640 ) {
 				$('#masthead').css('height', headerHeight);
 
@@ -24,47 +24,48 @@
 				$('#masthead').css('height', headerHeight + navHeight);
 			}
 		}
-	}
+	};
 
 	// Priority+ navigation, whee!
 	function priorityNav() {
-	  var navWidth = 0;
-	  var firstMoreElement = $('#more-menu li').first();
+		// Don't do it on the homepage, because the homepage is too damned delicate
+		if (!$('body').hasClass('page-template-page-templatestemplate-front-php')) {
+		  var navWidth = 0;
+		  var firstMoreElement = $('#more-menu li').first();
 
-	  // Calculate the width of our "more" containing element
-	  var moreWidth = $('#more-menu').outerWidth(true);
+		  // Calculate the width of our "more" containing element
+		  var moreWidth = $('#more-menu').outerWidth(true);
 
-	  // Calculate the current width of our navigation
-	  $('#site-navigation .menu span > li').each(function() {
-	      navWidth += $(this).outerWidth( true );
-	  });
+		  // Calculate the current width of our navigation
+		  $('#site-navigation .menu span > li').each(function() {
+		      navWidth += $(this).outerWidth( true );
+		  });
 
-	  // Calculate our available space
-	  var availableSpace = $('#site-navigation').outerWidth(true) - moreWidth;
+		  // Calculate our available space
+		  var availableSpace = $('#site-navigation').outerWidth(true) - moreWidth;
 
-	  // If our nav is wider than our available space, we're going to move items
-	  if (navWidth > availableSpace) {
-		var lastItem = $('#site-navigation .menu span > li:not(#more-menu)').last();
-		lastItem.attr('data-width', lastItem.outerWidth(true));
-		lastItem.prependTo($('#more-menu .sub-menu').eq(0));
-		priorityNav(); // Rerun this function!
+		  // If our nav is wider than our available space, we're going to move items
+		  if (navWidth > availableSpace) {
+			var lastItem = $('#site-navigation .menu span > li:not(#more-menu)').last();
+			lastItem.attr('data-width', lastItem.outerWidth(true));
+			lastItem.prependTo($('#more-menu .sub-menu').eq(0));
+			priorityNav(); // Rerun this function!
 
-		// But if we have the extra space, we should add the items back to our menu
-		} else if (navWidth + firstMoreElement.data('width') < availableSpace) {
-			// Check to be sure there's enough space for our extra element
-			firstMoreElement.insertBefore($('#site-navigation .menu span > li').last());
-			priorityNav();
-	  }
+			// But if we have the extra space, we should add the items back to our menu
+			} else if (navWidth + firstMoreElement.data('width') < availableSpace) {
+				// Check to be sure there's enough space for our extra element
+				firstMoreElement.insertBefore($('#site-navigation .menu span > li').last());
+				priorityNav();
+		  }
 
-	  // Hide our more-menu entirely if there's nothing in it
-	  if ($('#more-menu li').length > 0) {
-	    $('#more-menu').addClass('visible');
-	  } else {
-	    $('#more-menu').removeClass('visible');
-	  }
-	}
-
-	// Sticky nav
+		  // Hide our more-menu entirely if there's nothing in it
+		  if ($('#more-menu li').length > 0) {
+		    $('#more-menu').addClass('visible');
+		  } else {
+		    $('#more-menu').removeClass('visible');
+		  }
+	  } // check for body class
+  }; // function priorityNav
 
 	// Okay, now we want to stick-ify our menu when we reach it
 	function stickyNav() {
