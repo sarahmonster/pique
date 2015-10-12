4b0c0d9a94beb5f491e433b3157c1036c17c9ac8
sarah <sarah@triggersandsparks.com>
2015-10-12 02:19:26 +0100

    Use the correct default colour for header text.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -17,7 +17,7 @@
 function pique_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'pique_custom_header_args', array(
 		'default-image'          => esc_url( get_template_directory_uri() ) . '/assets/images/default-header.jpg',
-		'default-text-color'     => '000000',
+		'default-text-color'     => '#FCFBF9',
 		'width'                  => 1400,
 		'height'                 => 400,
 		'flex-height'            => true,
