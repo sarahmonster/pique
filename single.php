3788e86abed7d0e58d6553d48ee9c4ee25cd555b
sarah <sarah@triggersandsparks.com>
2015-10-28 14:42:19 +0100

    Improve look & feel of post navigation.

--- single.php
+++ single.php
@@ -14,7 +14,10 @@ get_header(); ?>
 
 			<?php get_template_part( 'components/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+			<?php the_post_navigation( array (
+										'prev_text' => '<span>' . esc_html__( 'Previous', 'pique' ) . '</span> %title',
+										'next_text' => '<span>' . esc_html__( 'Next', 'pique' ) . '</span> %title',
+									 ) ); ?>
 
 			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
